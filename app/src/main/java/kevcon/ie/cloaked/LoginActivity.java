e2e0d006abae70af2320a4b2c525113d02fdff87
kbarry91 <g00339811@gmit.ie>
2018-02-22 22:32:23 +0000

    added google-services.json
    Signed-off-by: kbarry91 <g00339811@gmit.ie>

--- app/src/main/java/kevcon/ie/cloaked/LoginActivity.java
+++ app/src/main/java/kevcon/ie/cloaked/LoginActivity.java
@@ -49,10 +49,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
-     */
+
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+     */
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -319,7 +320,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             } catch (InterruptedException e) {
                 return false;
             }
-
+/*
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
@@ -327,7 +328,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     return pieces[1].equals(mPassword);
                 }
             }
-
+*/
             // TODO: register the new account here.
             return true;
         }
