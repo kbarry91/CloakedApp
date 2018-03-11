42574ce786c4e721ae4bd96f160c5b2832138af2
kbarry91 <g00339811@gmit.ie>
2018-03-11 02:32:53 +0000

    added broadcast receiver to check sent status of sms, added title bar to later include back option
    Signed-off-by: kbarry91 <g00339811@gmit.ie>

--- app/src/main/java/kevcon/ie/cloaked/SendMessage.java
+++ app/src/main/java/kevcon/ie/cloaked/SendMessage.java
@@ -6,8 +6,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
@@ -15,10 +16,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 /**
  * @author kevin barry
  */
-public class SendMessage extends Activity {
+public class SendMessage extends AppCompatActivity {
     // Tag for debugging
     private static final String TAG = "SendMessage";
 
@@ -31,6 +33,11 @@ public class SendMessage extends Activity {
         Log.d(TAG, "onCreate: opened");
         setContentView(R.layout.activity_send_message);
 
+        // set up a toolbar and back button to parent activity
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        //getSupportActionBar().setHomeButtonEnabled(true);
         //assign user_message and button to view
         user_message = findViewById(R.id.edit_message);
         send_button = findViewById(R.id.button_sms_send);
@@ -51,7 +58,63 @@ public class SendMessage extends Activity {
         //String strMessage = "Hello this is a debug text";
         SmsManager sms = SmsManager.getDefault();
 
-        sms.sendTextMessage(strPhone, null, strMessage, null, null);
+
+        Context curContext = this.getApplicationContext();
+
+        // must create intents to Check if sms is sent and delivered
+        PendingIntent sentPending = PendingIntent.getBroadcast(curContext,
+                0, new Intent("SENT"), 0);
+
+        // receiver intent to return result of  Broadcast
+        curContext.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS Sent.",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "SMS Not Sent: Generic failure.",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "SMS Not Sent: No service ",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Not Sent: Null PDU.",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Not Sent: Ensure Airplane mode is disabled",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        }, new IntentFilter("SENT"));
+
+        PendingIntent deliveredPending = PendingIntent.getBroadcast(curContext,
+                0, new Intent("DELIVERED"), 0);
+
+        curContext.registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context arg0, Intent arg1) {
+                        switch (getResultCode()) {
+                            case Activity.RESULT_OK:
+                                Toast.makeText(getBaseContext(), "Delivered.",
+                                        Toast.LENGTH_LONG).show();
+                                break;
+                            case Activity.RESULT_CANCELED:
+                                Toast.makeText(getBaseContext(), "Not Delivered: Canceled.",
+                                        Toast.LENGTH_LONG).show();
+                                break;
+                        }
+                    }
+                }, new IntentFilter("DELIVERED"));
+
+        sms.sendTextMessage(strPhone, null, strMessage, sentPending, deliveredPending);
 
         // display notification of message sent
         Toast.makeText(this, "Sent.", Toast.LENGTH_SHORT).show();
