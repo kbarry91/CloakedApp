1f0e2d26a4820c228c79e965b0dea3f8632fc26f
kbarry91 <g00339811@gmit.ie>
2018-03-15 03:37:20 +0000

    implemented holder message view holders, need to bind data
    Signed-off-by: kbarry91 <g00339811@gmit.ie>

--- app/src/main/java/kevcon/ie/cloaked/MessageViewAdapter.java
+++ app/src/main/java/kevcon/ie/cloaked/MessageViewAdapter.java
@@ -2,6 +2,8 @@ package kevcon.ie.cloaked;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
@@ -23,8 +25,39 @@ public class MessageViewAdapter extends RecyclerView.Adapter {
         this.listMessageData = listMessageData;
     }
 
+
+    // getItemViewType returns 0 as default so must override to return a value for sent and received messages
+    @Override
+    public int getItemViewType(int position) {
+        Message message = listMessageData.get(position);
+        switch (message.getType()) {
+            case 1:
+                //type 1 is a received message
+                return 1;
+            case 2:
+                //type 2 is a sent message
+                return 2;
+            default:
+
+                return 0;
+        }
+    }
+
+    // must confirm if message is sent or received and assign the appropriate view
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+
+        if (viewType == 2) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_sent, parent, false);
+            return new messageOutHolder(view);
+        } else if (viewType == 1) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_recieved, parent, false);
+            return new messageInHolder(view);
+        }
+
         return null;
     }
 
@@ -36,5 +69,17 @@ public class MessageViewAdapter extends RecyclerView.Adapter {
     @Override
     public int getItemCount() {
         return listMessageData.size();
+    }
+
+    private class messageOutHolder extends RecyclerView.ViewHolder {
+        messageOutHolder(View view) {
+            super(view);
+        }
+    }
+
+    private class messageInHolder extends RecyclerView.ViewHolder {
+        messageInHolder(View view) {
+            super(view);
+        }
     }
 }
