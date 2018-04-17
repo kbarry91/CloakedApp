5b609047b69a1eb19be31bcc53502f506f4b14e6
conor <c-raftery@hotmail.com>
2018-04-17 02:26:47 +0100

    Added DrawerLayout to ContactsMainActivity

--- app/src/main/java/kevcon/ie/cloaked/ContactsMainActivity.java
+++ app/src/main/java/kevcon/ie/cloaked/ContactsMainActivity.java
@@ -1,8 +1,13 @@
 package kevcon.ie.cloaked;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -13,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import java.util.List;
 
@@ -20,7 +26,7 @@ import java.util.List;
  * Created by c-raf on 08/03/2018.
  */
 
-public class ContactsMainActivity extends Activity {
+public class ContactsMainActivity extends AppCompatActivity {
     Button contactAddButton;
     ListView listContacts;
 
@@ -33,10 +39,70 @@ public class ContactsMainActivity extends Activity {
     //database variables
     ContactsHelperDB myDb;
     List<Contacts> contactList;
+
+
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts_activity_main);
+
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
+        getSupportActionBar().setTitle("Contacts");
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        //SearchView search = (SearchView) item.getActionView();
+        //search.setLayoutParams(new ActionBar.LayoutParams(Gravity.RIGHT));
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
 
         // inilise database
         myDb = new ContactsHelperDB(this);
@@ -99,6 +165,15 @@ public class ContactsMainActivity extends Activity {
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
 
