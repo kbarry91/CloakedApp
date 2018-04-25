f7ec24ccf251b72cdbdefba50c9e7c2b6cb6879d
kbarry91 <g00339811@gmit.ie>
2018-04-25 22:20:22 +0100

    Code Clean-up,Code Inspect Complete - KeyController
    Signed-off-by: kbarry91 <g00339811@gmit.ie>

--- app/src/main/java/kevcon/ie/cloaked/KeyController.java
+++ app/src/main/java/kevcon/ie/cloaked/KeyController.java
@@ -16,33 +16,43 @@ import android.widget.Toast;
 import java.util.Random;
 
 /**
+ * <h1>KeyController</h1>
+ * KeyController is a class to hold key setting methods.
+ *
  * @author kevin barry
+ * @since 25/4/2018
  */
-
 public class KeyController extends Activity {
-    // Method to verify key
-    ContactsHelperDB myDb;
-    boolean keyWasSet;
-    String BRICK = "INBRICK.2.0>>>>>>>";
 
+    private ContactsHelperDB myDb;
+    private boolean keyWasSet;
 
+    /**
+     * setNewKey allows a new key to be set.
+     * The set key is updated in the SQL lite database.
+     *
+     * @param contact A Contact Object to set the key for.
+     * @param ctx     the Context of the view to return to.
+     * @param title   The title to display for the dialog.
+     */
     public void setNewKey(final Contacts contact, final Context ctx, String title) {
-        // final ContactsHelperDB myDb = new ContactsHelperDB();
+
         AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
         builder.setTitle(title);
 
-
-        // Set up the input
+        // Set up the input view
         View viewInflated = LayoutInflater.from(ctx).inflate(R.layout.key_set_dialog, null, false);
 
         final EditText input = viewInflated.findViewById(R.id.input);
-        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+
+        // Inflate the view.
         builder.setView(viewInflated);
 
         // Set up the buttons
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+
                 String keyEntered;
                 keyEntered = input.getText().toString();
 
@@ -51,32 +61,35 @@ public class KeyController extends Activity {
                     // set the key
                     contact.setKey(keyEntered);
                     contact.setKeySet(true);
-                    Log.d("TESTEDIT", contact.toString()
+                    Log.d("TEST KEY", contact.toString()
                     );
+
+                    // Initialize database helper .
                     myDb = new ContactsHelperDB(ctx);
-                    //returns true if key success
+
+                    // Returns true if key success.
                     if (myDb.editContact(contact)) {
                         Toast.makeText(ctx, "Key Set Success",
                                 Toast.LENGTH_LONG).show();
 
+                        // Send key request to contact.
                         Utils.sendMessage(contact, scrambleKey(contact), ctx);
-
-
 
                     } else {
                         Toast.makeText(ctx, "Could not set key at this time",
                                 Toast.LENGTH_LONG).show();
                     }
+                    // Close database
                     myDb.close();
 
                 } else {
                     Toast.makeText(ctx, "Key must be 4 or more characters!",
                             Toast.LENGTH_LONG).show();
                 }
+                // DEBUG
                 Log.d("ENTERED KEY in dialog", keyEntered);
 
                 dialog.dismiss();
-
 
             }
 
@@ -87,21 +100,23 @@ public class KeyController extends Activity {
                 dialog.cancel();
             }
         });
+        // Inflate the dialog.
         builder.show();
-
 
     }
 
     /**
-     * Scrambles a key within a message
+     * scrambleKey Scrambles a key within a message.
      *
-     * @return a key set message
+     * @param contact The Contact object the key is to be set for.
+     * @return a key set String
      */
-    public String scrambleKey(Contacts contact) {
+    private String scrambleKey(Contacts contact) {
         StringBuilder scrambledKey = new StringBuilder();
 
         String org = contact.getKey();
         Random r = new Random();
+
         // generate a random letter
         char c = (char) (r.nextInt(26) + 'a');
 
@@ -111,22 +126,26 @@ public class KeyController extends Activity {
             c = (char) (r.nextInt(26) + 'a');
         }
         Log.d("SCRAM", scrambledKey.toString());
-        // convert the scrambled key to hex
-        String hexKey =  Utils.keyStringToHex(scrambledKey.toString().getBytes());
-        return hexKey;
+
+        // convert the scrambled key to hex and return it.
+        return Utils.keyStringToHex(scrambledKey.toString().getBytes());
     }
 
     /**
-     * deScrambles a key from a message
+     * unScrambleKey deScrambles a key from a message.
      *
+     * @param newKeySet the new key set string.
      * @return a new key
      */
     public String unScrambleKey(String newKeySet) {
         StringBuilder deScrambledKey = new StringBuilder();
-       // final int prefixLength = "Please Open This In Cloaked:".length();
-       final String hexStr = newKeySet.substring(newKeySet.lastIndexOf(":")+1);
-      final  String keyStr = Utils.hexKeyToString(hexStr);
-        Log.d("SCRAMBLE KEY: hex:", hexStr+" str:"+keyStr);
+        // final int prefixLength = "Please Open This In Cloaked:".length();
+        final String hexStr = newKeySet.substring(newKeySet.lastIndexOf(":") + 1);
+        final String keyStr = Utils.hexKeyToString(hexStr);
+
+        // DEBUG
+        Log.d("SCRAMBLE KEY: hex:", hexStr + " str:" + keyStr);
+
         for (int i = 1; i < keyStr.length(); i += 2) {
             deScrambledKey.append(keyStr.charAt(i));
         }
@@ -138,15 +157,18 @@ public class KeyController extends Activity {
     /**
      * resetKeys displays a dialog of a newly requested key set.
      * If confirmed the new key is set
-     * Must be synchroised to stop program advancing before user makes a choice
+     * Must be synchronised to stop program advancing before user makes a choice
+     *
+     * @param contact      The Contact in use.
+     * @param keyText      The key set string.
+     * @param ctx          The context view to inflate from.
+     * @param lookValidKey A method that implements runnable to ensure key is valid.
+     * @return a boolean value for success/failure.
      */
     public boolean resetKey(final String keyText, final Contacts contact, final Context ctx, final Runnable lookValidKey) {
-        Log.e(BRICK, "just entered reset key");
-        // Run this function on a ui thread
 
-
-        Log.d(BRICK, "just RUNNNING");
         keyWasSet = false;
+
         //pop up dialog to display message
         final Dialog resetDialog = new Dialog(ctx);
 
@@ -158,9 +180,11 @@ public class KeyController extends Activity {
         // set the pop up text
         keyTextView.setText(keyText);
 
-        Log.d("RESETKEYPOP", "reset key" + keyText);
+        // DEBUG
+        Log.d("RESET_KEY_POP", "reset key" + keyText);
 
         Button confirm = resetDialog.findViewById(R.id.confirm_reset);
+
         // Close the dialog on click
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -209,10 +233,8 @@ public class KeyController extends Activity {
 
         resetDialog.show();
 
-
         return keyWasSet;
     }
-
 
 }
 
